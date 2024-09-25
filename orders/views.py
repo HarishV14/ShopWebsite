4cbab680d6bd856e1eb6c197a820fb40e82ed32c
HarishV14 <harishv@testpress.in>
2024-09-25 12:16:20 +0530

    Generated Pdf and Modified admin view

--- orders/views.py
+++ orders/views.py
@@ -26,3 +26,31 @@ def order_create(request):
     else:
         form = OrderCreateForm()
     return render(request,'orders/order/create.html',{'cart': cart, 'form': form})
+
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+# this are view created for the admin page to look the details of the seperate order
+# staff memeber is mention that only admin can route to that url
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,'admin/orders/order/detail.html',{'order': order})   
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+import weasyprint
+
+# this for the conversion of the html to pdf using wasyprint
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html',{'order': order})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(
+                                               settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
